00c27a45824713af0327f47a2c71541bfd1ef6cd
g00326216 <36736985+g00326216@users.noreply.github.com>
2018-03-15 15:26:34 +0000

    5. Define a paddle

--- js/breakout.js
+++ js/breakout.js
@@ -6,6 +6,11 @@ var y = canvas.height-30;
 var dx = 2;
 var dy = -2;
 var ballColour = "#0095DD"
+var paddleHeight = 10;
+var paddleWidth = 75;
+var paddleX = (canvas.width-paddleWidth)/2;
+var rightPressed = false;
+var leftPressed = false;
 
 function drawBall() {
 	ctx.beginPath();
@@ -14,21 +19,56 @@ function drawBall() {
 	ctx.fill();
 	ctx.closePath();
 }
+function drawPaddle() {
+	ctx.beginPath();
+	ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+	ctx.fillStyle = "#0095DD";
+	ctx.fill();
+	ctx.closePath();
+}
 function draw () {
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	drawBall();
+	drawPaddle();
 	x += dx;
 	y += dy;
+	
 	if(y+ dy > canvas.height-ballRadius || y+dy <ballRadius){
 		dy=-dy;
-		ctx.fillStyle ="red";
-		ctx.fill();
+		
 	}
 	if(x+dx > canvas.width-ballRadius || x+dx <ballRadius){
 		dx=-dx;
-		ctx.fillStyle = "red";
-		ctx.fill();
+		
 	}
-	
+	if(rightPressed && paddleX < canvas.width-paddleWidth) {
+		paddleX += 7;
+	}
+	else if(leftPressed && paddleX > 0) {
+		paddleX -= 7;
+	}
 }
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup" , keyUpHandler, false);
+
+function keyDownHandler(e) {
+	if(e.keyCode == 39) {
+		rightPressed = true;
+	}
+	else if(e.keyCode == 37) {
+		leftPressed = true;
+	}
+}
+
+function keyUpHandler(e) {
+	if(e.keyCode == 39) {
+		rightPressed = false;
+	}
+	else if(e.keyCode == 37) {
+		leftPressed = false;
+	}
+}
+
+
 setInterval(draw, 10);
